bf4658377e7285572d313c8f39d65fd22a1523ef
Tim <Tim.k.johnsen@gmail.com>
2023-03-21 18:07:44 -0700

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -3,16 +3,16 @@ from time import time
 # keeps track of laps along with final time
 class Stopwatch:
   def __init__(self):
-    self.start_time = time.time()
+    self.start_time = time()
     self.last_time = self.start_time
     self.laps = []
   def lap(self):
-    this_time = time.time()
+    this_time = time()
     delta_time = this_time - self.last_time
     self.laps.append(delta_time)
     self.last_time = this_time
     return delta_time
   def stop(self):
-    self.stop_time = time.time()
+    self.stop_time = time()
     self.delta_time = self.stop_time - self.start_time
     return self.delta_time
